71621b31c4a3aac01655d285d7b79495c6ed1e17
Anik Saha <aniksaha200r@gmail.com>
2023-05-24 00:35:50 +0600

    Hooking up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,69 +1,8 @@
-import React, { useState } from "react";
+import UseDisplayContent from "./Hooks/UseDisplayContent";
 import TablePages from "./Pages/TablePages";
-import { evaluate } from "mathjs";
 
 const App = () => {
-  const [currentContent, setCurrentContent] = useState("");
-  const addContent = (val) => {
-    if (currentContent === "Invalid syntex") {
-      setCurrentContent("");
-    }
-    if (val === "x") val = "*";
-
-    setCurrentContent(`${currentContent}${val}`);
-  };
-  const calculatedValue = () => {
-    let result = NaN;
-    try {
-      result = evaluate(currentContent);
-    } catch (error) {
-      result = "Invalid syntex";
-    }
-
-    return result;
-  };
-  const handleClick = (val) => {
-    const check = /(^[0-9])/;
-    if (val === "C") {
-      setCurrentContent("");
-    } else if (val === "DEL") {
-      let temp = currentContent;
-      try {
-        temp = temp.substring(0, temp.length - 1);
-      } catch (error) {
-        temp = "";
-      }
-      setCurrentContent(temp);
-    } else if (
-      val === "%" ||
-      val === "+" ||
-      val === "-" ||
-      val === "x" ||
-      val === "/" ||
-      val === "."
-    ) {
-      if (currentContent.length === 0) {
-        addContent(val);
-        return;
-      }
-      if (
-        currentContent[currentContent.length - 1] !== "%" &&
-        currentContent[currentContent.length - 1] !== "+" &&
-        currentContent[currentContent.length - 1] !== "-" &&
-        currentContent[currentContent.length - 1] !== "/" &&
-        currentContent[currentContent.length - 1] !== "x" &&
-        currentContent[currentContent.length - 1] !== "."
-      ) {
-        addContent(val);
-      }
-    } else if (val === "=") {
-      setCurrentContent(calculatedValue());
-    } else if (val === "SAVE") {
-      console.log("Saved");
-    } else if (check.test(val)) {
-      addContent(val);
-    }
-  };
+  const [currentContent, handleDataInput] = UseDisplayContent();
   return (
     <div className="m-36">
       <div className="w-64">
@@ -74,7 +13,7 @@ const App = () => {
           readOnly
         ></input>
       </div>
-      <TablePages handleClick={handleClick} />
+      <TablePages handleClick={handleDataInput} />
     </div>
   );
 };
--- my-app/src/Hooks/UseDisplayContent.js
+++ my-app/src/Hooks/UseDisplayContent.js
@@ -1,8 +1,72 @@
-import { useContext } from "react";
-import DisplayContentContext from "../Context/DisplayContent";
+import { useState } from "react";
+import { evaluate } from "mathjs";
 
 const UseDisplayContent = () => {
-  return useContext(DisplayContentContext);
+  const [currentContent, setCurrentContent] = useState("");
+  const addContent = (val) => {
+    if (currentContent === "Invalid syntex") {
+      setCurrentContent("");
+    }
+    if (val === "x") val = "*";
+
+    setCurrentContent(`${currentContent}${val}`);
+  };
+  const calculatedValue = () => {
+    let result = NaN;
+    try {
+      result = evaluate(currentContent);
+    } catch (error) {
+      result = "Invalid syntex";
+    }
+
+    return result;
+  };
+  const handleDataInput = (val) => {
+    const check = /(^[0-9])/;
+    if (val === "C") {
+      setCurrentContent("");
+    } else if (val === "DEL") {
+      let temp = currentContent;
+      try {
+        temp = temp.substring(0, temp.length - 1);
+      } catch (error) {
+        temp = "";
+      }
+      setCurrentContent(temp);
+    } else if (
+      val === "%" ||
+      val === "+" ||
+      val === "-" ||
+      val === "x" ||
+      val === "/" ||
+      val === "."
+    ) {
+      if (currentContent.length === 0) {
+        addContent(val);
+        return;
+      }
+      if (
+        currentContent[currentContent.length - 1] !== "%" &&
+        currentContent[currentContent.length - 1] !== "+" &&
+        currentContent[currentContent.length - 1] !== "-" &&
+        currentContent[currentContent.length - 1] !== "/" &&
+        currentContent[currentContent.length - 1] !== "x" &&
+        currentContent[currentContent.length - 1] !== "."
+      ) {
+        addContent(val);
+      }
+    } else if (val === "=") {
+      setCurrentContent(calculatedValue());
+    } else if (val === "SAVE") {
+      console.log("Saved");
+    } else if (check.test(val)) {
+      addContent(val);
+    }
+  };
+  return {
+    currentContent,
+    handleDataInput,
+  };
 };
 
 export default UseDisplayContent;
